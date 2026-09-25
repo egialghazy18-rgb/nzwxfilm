9768a8888ce2d218dd0cfdd6a329375a938860b5
egialghazy18-rgb <egialghazy18@gmail.com>
2026-09-25 14:12:05 +0700

    fix: disable zoom

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,13 @@ export const metadata: Metadata = {
   description: "Nonton film dan series favorit kamu",
 };
 
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="id">
